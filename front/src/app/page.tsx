37a7befea22ff8e3fe26c649ac21cb4ac9315f1d
Andrey Smirnov <sa@skb-techno.kz>
2026-08-05 22:13:39 +0500

    test: trigger Dima frontend deployment

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { CoachHome } from "@/components/CoachHome";
 import { getCurrentUser } from "@/lib/auth";
 
+
 export default async function Home() {
   const user = await getCurrentUser();
   if (!user) redirect("/login");
