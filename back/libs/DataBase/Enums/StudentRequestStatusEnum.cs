6d04f8d83ceb29ebd937884f5f5b7956f7ce82c6
Andrey Smirnov <sa@skb-techno.kz>
2026-08-15 13:00:35 +0500

    Подписал Enum

--- back/libs/DataBase/Enums/StudentRequestStatusEnum.cs
+++ back/libs/DataBase/Enums/StudentRequestStatusEnum.cs
@@ -5,7 +5,18 @@ namespace JiuDiary.Database.Enums;
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum StudentRequestStatusEnum
 {
+    /// <summary>
+    /// На рассмотрении
+    /// </summary>
     Pending = 1,
+
+    /// <summary>
+    /// Принята
+    /// </summary>
     Accepted = 2,
+
+    /// <summary>
+    /// Отклоенена
+    /// </summary>
     Rejected = 3
 }
